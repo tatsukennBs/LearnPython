c7325501e958b27e3a08699ab7e18aef1e305ff1
tatsukennBs <tatsunori.palmeaves@gmail.com>
2024-02-25 14:37:18 +0900

    delete testfile

--- pytestmain.py
+++
@@ -1,9 +0,0 @@
-from pytestlib import SampleIot
-
-def main():
-    print("Stating")
-    device_client = SampleIot.connectionDevices
-    print(device_client)
-
-if __name__ == "__main__":
-    main()
